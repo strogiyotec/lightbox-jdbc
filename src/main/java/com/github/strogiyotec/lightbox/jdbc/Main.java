b82702807987e40b70f2b2913045e1b7e19475a0
Lollipop <lollipop@devadmin.com>
2018-08-19 22:25:47 +0600

    add insert

--- src/main/java/com/github/strogiyotec/lightbox/jdbc/Main.java
+++ src/main/java/com/github/strogiyotec/lightbox/jdbc/Main.java
@@ -5,6 +5,12 @@ import com.github.strogiyotec.lightbox.jdbc.session.DriverSession;
 import com.github.strogiyotec.lightbox.jdbc.stmnt.Select;
 import com.github.strogiyotec.lightbox.jdbc.stmnt.SingleSelect;
 
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public final class Main {
 
     private Main() {
@@ -13,7 +19,19 @@ public final class Main {
 
     public static void main(final String[] args) throws Exception {
         final Session postgres = new DriverSession("jdbc:postgresql://127.0.0.1:5432/test", "postgres", "123");
-        final Select select = new Select(postgres, new SimpleQuery("select m.* , mi.* from movie m inner join movie_info mi on m.id = mi.movie_id"));
-        select.result().get();
+        /*final Select select = new Select(postgres, new SimpleQuery("select m.* , mi.* from movie m inner join movie_info mi on m.id = mi.movie_id"));
+        select.result().get();*/
+        Connection connection = postgres.connection();
+        final PreparedStatement statement = connection.prepareStatement(String.join("\n", "insert into people",
+                "(login,paasword)",
+                "values" +
+                        "(?,?)"), Statement.RETURN_GENERATED_KEYS);
+        statement.setString(1,"ALmas");
+        statement.setString(2,"qwerty");
+        statement.execute();
+        final ResultSet generatedKeys = statement.getGeneratedKeys();
+        while(generatedKeys.next()){
+            System.out.println(generatedKeys.getInt(1));
+        }
     }
 }
