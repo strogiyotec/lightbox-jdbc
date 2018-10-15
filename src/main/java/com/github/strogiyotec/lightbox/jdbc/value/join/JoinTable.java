b94294529cacca414aed85774cbfebdaa5e54c65
Lollipop <lollipop@devadmin.com>
2018-10-15 17:23:24 +0600

    remove unused list from JoinTable

--- src/main/java/com/github/strogiyotec/lightbox/jdbc/value/join/JoinTable.java
+++ src/main/java/com/github/strogiyotec/lightbox/jdbc/value/join/JoinTable.java
@@ -2,23 +2,16 @@ package com.github.strogiyotec.lightbox.jdbc.value.join;
 
 import com.github.strogiyotec.lightbox.jdbc.Table;
 
-import java.util.Arrays;
-import java.util.List;
-
 public final class JoinTable implements Table {
 
     private final String name;
 
-    private final List<String> joinBy;
-
-    public JoinTable(final String name, final List<String> joinBy) {
+    public JoinTable(final String name) {
         this.name = name;
-        this.joinBy = joinBy;
     }
 
     public JoinTable(final CharSequence name, final String... joinBy) {
         this.name = name.toString();
-        this.joinBy = Arrays.asList(joinBy);
     }
 
     @Override
