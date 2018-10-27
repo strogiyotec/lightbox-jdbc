6d074d123da5fc142a1189d35db0e545dd866f73
Lollipop <lollipop@devadmin.com>
2018-10-28 00:45:06 +0600

    add insert test

--- src/test/java/lightbox/jdbc/stmnt/InsertTest.java
+++ src/test/java/lightbox/jdbc/stmnt/InsertTest.java
@@ -3,59 +3,99 @@ package lightbox.jdbc.stmnt;
 import com.github.strogiyotec.lightbox.jdbc.Session;
 import com.github.strogiyotec.lightbox.jdbc.query.KeyedQuery;
 import com.github.strogiyotec.lightbox.jdbc.query.SimpleQuery;
+import com.github.strogiyotec.lightbox.jdbc.script.SqlScript;
 import com.github.strogiyotec.lightbox.jdbc.session.DriverSession;
 import com.github.strogiyotec.lightbox.jdbc.stmnt.KeyedUpdate;
 import com.github.strogiyotec.lightbox.jdbc.stmnt.Update;
 import com.github.strogiyotec.lightbox.jdbc.value.data.StringValue;
+import lightbox.jdbc.fake.FakeDatabaseSession;
+import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Ignore;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.Iterator;
 import java.util.Map;
 
-@Ignore
+import static org.hamcrest.CoreMatchers.is;
+
+
 public final class InsertTest extends Assert {
+
+    private static final Session postgres = new FakeDatabaseSession();
+
+    @BeforeClass
+    public static void initTable() throws Exception {
+        new SqlScript(
+                postgres,
+                String.join(
+                        " ",
+                        "CREATE TABLE testInsert (",
+                        "id serial primary key ,",
+                        "name text not null )"
+                ),
+                String.join(
+                        " ",
+                        "INSERT INTO testInsert ",
+                        "(name) ",
+                        "VALUES ",
+                        "('Almas')"
+                ),
+                String.join(
+                        " ",
+                        "INSERT INTO testInsert ",
+                        "(name) ",
+                        "VALUES ",
+                        "('Murat')"
+                )
+        ).execute();
+    }
+
+    @AfterClass
+    public static void deleteTable() throws Exception {
+        new SqlScript(
+                postgres,
+                "DROP TABLE testInsert"
+        ).execute();
+    }
+
 
     @Test
     public void testInsert() throws Exception {
-        final Session postgres = new DriverSession("jdbc:postgresql://127.0.0.1:5432/test", "postgres", "123");
         final Update insert = new Update(
                 postgres,
                 new SimpleQuery(
                         String.join("\n",
-                                "insert into people",
-                                "(login,paasword)",
+                                "insert into testInsert",
+                                "(name)",
                                 "values",
-                                "(:name,:password)"),
-                        new StringValue("name", "Almas"),
-                        new StringValue("password", "qwerty"))
+                                "(:name)"),
+                        new StringValue("name", "Almas")
+                )
         );
         final Integer res = insert.result().get();
-        assertTrue(res != 0);
+        assertTrue(res == 1);
     }
 
     @Test
     public void insertWithKeys() throws Exception {
-        final Session postgres = new DriverSession("jdbc:postgresql://127.0.0.1:5432/test", "postgres", "123");
-        final KeyedUpdate keyedUpdate = new KeyedUpdate(
+        final KeyedUpdate insert = new KeyedUpdate(
                 postgres,
                 new KeyedQuery(
                         String.join("\n",
-                                "insert into people",
-                                "(login,paasword)",
+                                "insert into testInsert",
+                                "(name)",
                                 "values",
-                                "(:name,:password)"),
-                        new StringValue("name", "Almas"),
-                        new StringValue("password", "qwerty")
+                                "(:name)"),
+                        new StringValue("name", "Almas")
                 )
         );
-        final Iterator<Map<String, Object>> iterator = keyedUpdate.result().get().iterator();
-        boolean keysArePresent = false;
-        while (iterator.hasNext()) {
-            iterator.next().forEach((k, v) -> System.out.println(k + " " + v));
-            keysArePresent = true;
-        }
-        assertTrue(keysArePresent);
+        final Iterator<Map<String, Object>> iterator = insert.result().get().iterator();
+
+        assertTrue(iterator.hasNext());
+
+        final Map<String, Object> next = iterator.next();
+
+        assertThat(next.get("name"), is("Almas"));
     }
 }
