523e01a102783b1f589c94cf9fdb50c65ca470b6
Lollipop <lollipop@devadmin.com>
2018-10-28 17:37:02 +0600

    add value test

--- src/test/java/lightbox/jdbc/stmnt/ResultAsValueTest.java
+++ src/test/java/lightbox/jdbc/stmnt/ResultAsValueTest.java
@@ -1,29 +1,65 @@
 package lightbox.jdbc.stmnt;
 
 import com.github.strogiyotec.lightbox.jdbc.Result;
-import com.github.strogiyotec.lightbox.jdbc.Rows;
 import com.github.strogiyotec.lightbox.jdbc.Session;
 import com.github.strogiyotec.lightbox.jdbc.query.SimpleQuery;
+import com.github.strogiyotec.lightbox.jdbc.script.SqlScript;
 import com.github.strogiyotec.lightbox.jdbc.session.DriverSession;
 import com.github.strogiyotec.lightbox.jdbc.stmnt.ResultAsValue;
 import com.github.strogiyotec.lightbox.jdbc.stmnt.Select;
 import com.github.strogiyotec.lightbox.jdbc.value.data.IntValue;
-import com.google.common.collect.Iterables;
+import lightbox.jdbc.fake.FakeDatabaseSession;
+import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Ignore;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-@Ignore
 public final class ResultAsValueTest extends Assert {
+
+    private static final Session postgres = new FakeDatabaseSession();
+
+    @BeforeClass
+    public static void initTable() throws Exception {
+        new SqlScript(
+                postgres,
+                String.join(
+                        " ",
+                        "CREATE TABLE valueTest (",
+                        "id serial primary key ,",
+                        "name text not null )"
+                ),
+                String.join(
+                        " ",
+                        "INSERT INTO valueTest ",
+                        "(name) ",
+                        "VALUES ",
+                        "('Almas')"
+                ),
+                String.join(
+                        " ",
+                        "INSERT INTO valueTest ",
+                        "(name) ",
+                        "VALUES ",
+                        "('Murat')"
+                )
+        ).execute();
+    }
+
+    @AfterClass
+    public static void deleteTable() throws Exception {
+        new SqlScript(
+                postgres,
+                "DROP TABLE valueTest"
+        ).execute();
+    }
 
     @Test(expected = IllegalStateException.class)
     public void throwExceptionOnEmptyResultSet() throws Exception {
-        final Session postgres = new DriverSession("jdbc:postgresql://127.0.0.1:5432/test", "postgres", "123");
         final ResultAsValue<String> id = new ResultAsValue<>(
                 new Select(
                         postgres,
                         new SimpleQuery(
-                                "select id from child where id = :id ",
+                                "select id from valueTest where id = :id ",
                                 new IntValue("id", -321)
                         )
                 ), String.class
@@ -34,51 +70,29 @@ public final class ResultAsValueTest extends Assert {
     }
 
     @Test
-    public void insertInSelectStatement() throws Exception {
-        final Session postgres = new DriverSession("jdbc:postgresql://127.0.0.1:5432/test", "postgres", "123");
-        final Select select =
-                new Select(
-                        postgres,
-                        new SimpleQuery(
-                                String.join(
-                                        " ",
-                                        "insert into child (par_id)",
-                                        "values",
-                                        "(:id)"
-                                ),
-                                new IntValue("id", -321)
-                        )
-                );
-        final Rows maps = select.result().get();
-        assertTrue(Iterables.isEmpty(maps));
-    }
-
-    @Test
     public void shouldGetSingleResult() throws Exception {
-        final Session postgres = new DriverSession("jdbc:postgresql://127.0.0.1:5432/test", "postgres", "123");
         final ResultAsValue<Integer> id = new ResultAsValue<>(
                 new Select(
                         postgres,
                         new SimpleQuery(
-                                "select id from child where id = :id ",
-                                new IntValue("id", 7)
+                                "select id from valueTest where id = :id ",
+                                new IntValue("id", 1)
                         )
                 ), Integer.class
 
         );
         final Result<Integer> result = id.result();
-        assertTrue(result.get() != null);
+        assertTrue(result.get() == 1);
     }
 
     @Test(expected = ClassCastException.class)
     public void throwExceptionOnIllegalCast() throws Exception {
-        final Session postgres = new DriverSession("jdbc:postgresql://127.0.0.1:5432/test", "postgres", "123");
         final ResultAsValue<String> id = new ResultAsValue<>(
                 new Select(
                         postgres,
                         new SimpleQuery(
-                                "select id from child where id = :id ",
-                                new IntValue("id", 7)
+                                "select id from valueTest where id = :id ",
+                                new IntValue("id", 1)
                         )
                 ), String.class
 
